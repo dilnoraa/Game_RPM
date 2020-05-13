9d1bee2e6369f5cb733f9c02680fb04259785544
Dilnora Matniyazova <dilnora.matniyazova@gmail.com>
2020-05-13 23:01:44 +0200

    Update FeededPlayer.java

--- Game_RPM/src/GameRPM/FeededPlayer.java
+++ Game_RPM/src/GameRPM/FeededPlayer.java
@@ -4,62 +4,59 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FeededPlayer extends Player{
+public class FeededPlayer extends Player {
 
-	int index=0;
-	private String text;
-	
-	public FeededPlayer(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	public void readFile() throws IOException {
-	     FileReader fileReader = new FileReader("sample_input.txt");
-	     String line;
-	     char letter;
-	     
-			BufferedReader br = new BufferedReader(fileReader);
+    int index = 0;
+    private String text;
 
-			while ((line = br.readLine()) != null) {
+    public FeededPlayer(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
 
-				text=line;
-			  
 
-			}
-			
-	       
-			letter=text.charAt(0);
-	        if (letter=='P')
-	        	move="Paper";
-			  else if(letter=='S')
-				  move="Scissors";
-			  else move="Rock";
-	        
-	        
-			br.close();
-		}
-	
+    public void readFile() throws IOException {
+        FileReader fileReader = new FileReader("sample_input.txt");
+        String line;
+        char letter;
 
-	@Override
-	
-	public String strategic_play(String str) {
-		  index++;
-		  
-		  char letter=text.charAt(index);
-		
-		  if (letter=='P')
-			  move="Paper";
-		  else if(letter=='S')
-			  move="Scissors";
-		  else move="Rock";
-		  
-		return move;
-	
-	}
-	
+        BufferedReader br = new BufferedReader(fileReader);
+
+        while ((line = br.readLine()) != null) {
+
+            text = line;
+
+
+        }
+
+
+        letter = text.charAt(0);
+        if (letter == 'P')
+            move = "Paper";
+        else if (letter == 'S')
+            move = "Scissors";
+        else move = "Rock";
+
+
+        br.close();
+    }
+
+
+    @Override
+    public String strategic_play(String str) {
+        index++;
+
+        char letter = text.charAt(index);
+
+        if (letter == 'P')
+            move = "Paper";
+        else if (letter == 'S')
+            move = "Scissors";
+        else move = "Rock";
+
+        return move;
+
+    }
+
 
 }
-
-
